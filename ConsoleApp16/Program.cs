5ca4fc4c899fc8df6ff4456bf33ac9b1e839deeb
exuteboll <exuteboll4@gmail.com>
2025-06-05 11:02:33 +0300

    Add files via upload

---
+++ ConsoleApp16/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp16
+{
+    class Program
+    {
+        enum Days
+        {
+            Январь, Февраль, Март ,Апрель, Май, Июнь, Июль, Август, Сентябрь, Октябрь, Ноябрь, Декабрь 
+        }
+        static void Main(string[] args)
+        {
+            Days i;
+            for (i = Days.Январь; i <= Days.Декабрь;i++) 
+                Console.WriteLine((int)i + " " + i);
+            Console.ReadKey();
+        }
+    }
+}
