c912027e472a96d09d3545383d156c50606696fa
exuteboll <exuteboll4@gmail.com>
2025-06-05 11:03:11 +0300

    Add files via upload

---
+++ ConsoleApp17/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp17
+{
+    class Program
+    {
+        enum Days
+        {
+            понедельник, вторник, среда, четверг, пятница, суббота, воскресенье
+        }
+        static void Main(string[] args)
+        {
+            Days i;
+            for (i = Days.понедельник; i <= Days.воскресенье; i++)
+                Console.WriteLine(i + " имеет значение " + (int)i);
+            Console.ReadKey();
+        }
+    }
+}
