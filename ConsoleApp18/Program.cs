9e1db163b3e4980f730cb700e3a4f52484ea82f8
exuteboll <exuteboll4@gmail.com>
2025-06-05 11:03:04 +0300

    Add files via upload

---
+++ ConsoleApp18/Program.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp18
+{
+    class Program
+    {
+        enum Operation
+        {
+            add, substract, multiply, divide
+        }
+       
+    
+            static void Main(string[] args)
+        {
+            double x =  15;
+            double y=  5;
+            double result= 0 ;
+            Operation op = Operation.divide;
+
+            if(op == Operation.add)
+            {
+                result = x + y;
+            }
+            else if ( op == Operation.divide)
+            {
+                result = x / y;
+
+            }
+            else if (op == Operation.multiply)
+            {
+                result = x * y;
+            }
+            else if (op == Operation.substract)
+            {
+                result = x - y;
+            }
+            Console.WriteLine(result);
+            Console.ReadKey();
+        }
+    }
+}
